8ab48b95e30abea59883854c0ed8f470c4208c64
Sagar <sagar.gp@xylem.com>
2024-10-03 10:06:22 -0400

    Initial commit

---
+++ Calculate/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculate;
+
+Console.WriteLine("Hello, World!");
+
+
+var temp = new Calculate_ModelClass();
+int a =10;
+int b =20;
+
+Console.WriteLine( "Addition: {0} ", temp.Add(10,20));
